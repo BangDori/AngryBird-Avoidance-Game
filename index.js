2f0f250a8cfb7986d4cde1b0629e9d19ac8fab58
BangDori <qudwns8865@naver.com>
2022-12-29 17:52:19 +0900

    Edit: Change Router Method

--- index.js
+++ index.js
@@ -1,38 +1,63 @@
+/*
 const http = require("http");
-const express = require("express");
 const fs = require("fs");
-const app = express();
 const server = http.createServer(app);
-const port = 3000;
-
 const WEBPATH = "./templates";
-
-app.use(express.static("public"));
-
 app.get("/", (req, res) => {
     fs.readFile(`${WEBPATH}/login.html`, (error, data) => {
         if(error) {
             console.log(err);
             return res.status(500).send("<h1>500 Error</h1>");
         }
-
         res.writeHead(200, { "Content-Type": "text/html" });
         res.end(data);
     })
 })
-
 app.post("/index", (req, res) => {
     fs.readFile(`${WEBPATH}/index.html`, (err, data) => {
         if(err) {
             console.log(err);
             return res.status(500).send("<h1>500 Error</h1>");
         }
+        const userName = req.params("user_name");
+        console.log(userName);
 
         res.writeHead(200, { "Content-Type": "text/html" });
+        res.send(userName);
         res.end(data);
     })
 })
-
 server.listen(port, () => {
     console.log(`Server running on ${port}`);
 })
+*/
+
+const express = require("express");
+const app = express();
+const path = require("path");
+const router = express.Router();
+var bodyParser = require('body-parser');
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
+app.engine('html', require('ejs').renderFile)
+app.set("view engine", "html");
+app.set("views", path.join(__dirname, "/views"));
+
+router.get("/", (req, res) => {
+    res.render("login");
+})
+
+router.post("/index", (req, res) => {
+    const name = req.body.name;
+
+    res.render("index", { user_name: name})
+})
+
+app.use("/", router);
+app.listen(3000);
+
+console.log("Running at Port 3000");
