2c53a27bdd090ff3afac7150c7de4e07fb6b1453
BangDori <qudwns8865@naver.com>
2022-12-30 15:43:51 +0900

    Edit: Integrate to Init.ts

--- public/js/join.js
+++ public/js/join.js
@@ -1,14 +1,14 @@
 "use strict";
-const JOIN = document.querySelector("#join");
-function init() {
-    JOIN === null || JOIN === void 0 ? void 0 : JOIN.addEventListener("click", (e) => {
-        const userName = document.querySelector("#naming").value;
-        console.log(userName.length);
-        if (userName.length < 1 || userName.length > 8) {
-            alert('Input Name length: 0~8');
-            e.preventDefault();
-            e.stopPropagation();
-        }
-    });
-}
-init();
+// const JOIN = document.querySelector("#join");
+// function init() {
+//     JOIN?.addEventListener("click", (e) => {
+//         const userName = (<HTMLInputElement>document.querySelector("#naming")).value;
+//         console.log(userName.length);
+//         if(userName.length < 1 || userName.length > 8) {
+//             alert('Input Name length: 0~8');
+//             e.preventDefault();
+//             e.stopPropagation();    
+//         }
+//     });
+// }
+// init();
--- public/js/join.ts
+++ public/js/join.ts
@@ -1,16 +1,16 @@
-const JOIN = document.querySelector("#join");
+// const JOIN = document.querySelector("#join");
 
-function init() {
-    JOIN?.addEventListener("click", (e) => {
-        const userName = (<HTMLInputElement>document.querySelector("#naming")).value;
+// function init() {
+//     JOIN?.addEventListener("click", (e) => {
+//         const userName = (<HTMLInputElement>document.querySelector("#naming")).value;
 
-        console.log(userName.length);
+//         console.log(userName.length);
 
-        if(userName.length < 1 || userName.length > 8) {
-            alert('Input Name length: 0~8');
-            e.preventDefault();
-            e.stopPropagation();    
-        }
-    });
-}
-init();
+//         if(userName.length < 1 || userName.length > 8) {
+//             alert('Input Name length: 0~8');
+//             e.preventDefault();
+//             e.stopPropagation();    
+//         }
+//     });
+// }
+// init();
