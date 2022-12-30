be74645eb18d36556232639dea1c55012beafca0
BangDori <qudwns8865@naver.com>
2022-12-30 20:55:32 +0900

    Add: character class & functions

--- public/js/character.js
+++ public/js/character.js
@@ -1,1 +1,57 @@
 "use strict";
+const circle = document.querySelector("#character");
+const speed = 5;
+var x_pos, y_pos;
+// Player 명 작성
+var player;
+function makePlayer() {
+    var bodyRect = document.body.getBoundingClientRect(), circleRect = circle === null || circle === void 0 ? void 0 : circle.getBoundingClientRect();
+    if (circleRect instanceof DOMRect) {
+        y_pos = circleRect.top - bodyRect.top;
+        x_pos = circleRect.left - bodyRect.left;
+    }
+    player = new Player("player", x_pos, y_pos);
+    console.log("Player Created!");
+    console.log(`Player pos(x: ${player.x_pos}, y: ${player.y_pos})`);
+}
+class Player {
+    constructor(name, x_pos, y_pos) {
+        this.name = name;
+        this.x_pos = x_pos;
+        this.y_pos = y_pos;
+        this.record = [];
+    }
+    insertRecord(record) {
+        this.record.push(record);
+        if (this.record.length > 1) {
+            this.record.sort();
+        }
+        console.log(this.record);
+    }
+    static movePlayer(e) {
+        // console.log(e.key);
+        switch (e.key) {
+            // ArrowRight
+            case 'ArrowRight':
+                x_pos += speed;
+                break;
+            // ArrowLeft
+            case 'ArrowLeft':
+                x_pos -= speed;
+                break;
+            // ArrowUp
+            case 'ArrowUp':
+                y_pos -= speed;
+                break;
+            // ArrowDown
+            case 'ArrowDown':
+                y_pos += speed;
+                break;
+        }
+        if (circle) {
+            circle.style.left = `${x_pos}px`;
+            circle.style.top = `${y_pos}px`;
+        }
+        console.log(`x: ${x_pos}, y: ${y_pos}`);
+    }
+}
--- public/js/character.ts
+++ public/js/character.ts
@@ -1,5 +1,60 @@
-interface Character {
-    name: string,
-    hp: number,
-    speed: number,
+const circle = document.querySelector<HTMLElement>("#character");
+const speed = 5;
+var x_pos:number, y_pos:number;
+
+// Player 명 작성
+var player: Player;
+
+function makePlayer() {    
+    var bodyRect = document.body.getBoundingClientRect(),
+        circleRect = circle?.getBoundingClientRect();
+
+    if(circleRect instanceof DOMRect) {
+        y_pos = circleRect.top - bodyRect.top;
+        x_pos = circleRect.left - bodyRect.left;
+    }
+
+    player = new Player("player", x_pos, y_pos);
+    console.log("Player Created!");
+    console.log(`Player pos(x: ${player.x_pos}, y: ${player.y_pos})`);
+}
+
+class Player {
+    public record: number[];
+    constructor(
+        public name: string,
+        public x_pos: number,
+        public y_pos: number,
+    ){
+        this.record = [];
+    }
+
+    public insertRecord(record: number) {
+        this.record.push(record);
+
+        if(this.record.length > 1) {
+            this.record.sort();
+        }
+        console.log(this.record);
+    }
+
+    static movePlayer(e: any) {
+        // console.log(e.key);
+        switch(e.key) {
+            // ArrowRight
+            case 'ArrowRight': x_pos += speed; break;
+            // ArrowLeft
+            case 'ArrowLeft': x_pos -= speed; break;
+            // ArrowUp
+            case 'ArrowUp': y_pos -= speed; break;
+            // ArrowDown
+            case 'ArrowDown': y_pos += speed; break;
+        }
+
+        if(circle) {
+            circle.style.left = `${x_pos}px`;
+            circle.style.top = `${y_pos}px`;    
+        }
+        console.log(`x: ${x_pos}, y: ${y_pos}`);
+    }
 }
