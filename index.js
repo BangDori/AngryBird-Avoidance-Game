c80815822344a457c140209871720c124c08b226
BangDori <qudwns8865@naver.com>
2023-01-02 18:55:19 +0900

    Fix: remove code

--- index.js
+++ index.js
@@ -47,9 +47,9 @@ app.engine('html', require('ejs').renderFile)
 app.set("view engine", "html");
 app.set("views", path.join(__dirname, "/views"));
 
-router.get("https://venerable-melba-bb7ad5.netlify.app/", (req, res) => {
-    res.render("login");
-})
+// router.get("https://venerable-melba-bb7ad5.netlify.app/", (req, res) => {
+//     res.render("login");
+// })
 
 router.get("/", (req, res) => {
     res.render("login");
